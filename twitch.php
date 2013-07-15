95746c4d472d16304c56ba86cd8fd343a70cc01e
b2un0 <branko.wilhelm@gmail.com>
2013-07-15 19:55:53 +0200

    update stream added first release

--- twitch.php
+++ twitch.php
@@ -12,24 +12,24 @@ defined('_JEXEC') or die;
 class plgContentTwitch extends JPlugin {
 
 	public function onContentPrepare($context, &$row, &$params, $page = 0) {
-        
+	
 		if (JString::strpos($row->text, '{twitch') === false){
 			return true;
 		}
-        
-        preg_match_all('#{twitch\s+(.*?)}#i', $row->text, $matches, PREG_SET_ORDER);
-        
+		
+		preg_match_all('#{twitch\s+(.*?)}#i', $row->text, $matches, PREG_SET_ORDER);
+		
 		if (!empty($matches)){
-            $doc = JFactory::getDocument();
-            $doc->addScript('media/twitch/twitch.js');
-            
+			$doc = JFactory::getDocument();
+			$doc->addScript('media/twitch/twitch.js');
+			
 			foreach ($matches as $match) {
-                $users[] = $match[1] = trim($match[1]);
-                $replace = '<div class="twitch" id="twitch_' . $match[1] . '">' . JHtml::_('link', 'http://www.twitch.tv/' . $match[1], $match[1], array('target' => '_blank')) . ' Offline</div>';
-                $row->text = JString::str_ireplace($match[0], $replace, $row->text);
-            }
-            
-            $doc->addScriptDeclaration('window.twitch.init("' . implode(',', $users) . '");');
-        }
+				$users[] = $match[1] = trim($match[1]);
+				$replace = '<div class="twitch" id="twitch_' . $match[1] . '">' . JHtml::_('link', 'http://www.twitch.tv/' . $match[1], $match[1], array('target' => '_blank')) . ' Offline</div>';
+				$row->text = JString::str_ireplace($match[0], $replace, $row->text);
+			}
+			
+			$doc->addScriptDeclaration('window.twitch.init("' . implode(',', $users) . '");');
+		}
 	}
 }
